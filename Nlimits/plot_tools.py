1afd60e4aed69e75acce95c88acf7d19cbb46de9
josuhg <josu.hernandez@ttk.elte.hu>
2023-03-10 12:22:03 +0100

    Updated version

--- Nlimits/plot_tools.py
+++ Nlimits/plot_tools.py
@@ -1,22 +1,30 @@
-import scipy 
+import scipy
 from scipy.interpolate import splprep, splev
 
 import numpy as np
 
-import seaborn as sns
 import colorsys
 
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import rc, rcParams
-from matplotlib.pyplot import *
-from matplotlib.pyplot import cm
 import matplotlib.tri as tri
 import matplotlib.patches as patches
 import matplotlib.colors as mc
-
+from matplotlib.pyplot import cm
 
 def log_interp1d(xx, yy, kind='linear', **kwargs):
+    """ Return an interpolating function using Scipy's interp1d for log-spaced data
+
+    Args:
+        xx (ndarray): x-axis data
+        yy (ndarray): y-axis data
+        kind (str, optional): Interpolating method to use. Defaults to 'linear'.
+
+    Returns:
+        scipy.interpolate._interpolate.interp1d: interpolating function.
+    """
+    
     logx = np.log10(xx)
     logy = np.log10(yy)
     lin_interp = scipy.interpolate.interp1d(logx, logy, kind=kind, **kwargs)
@@ -55,86 +63,149 @@ def std_fig(ax_form=std_axes_form,
 def std_savefig(fig, path, dpi=400, **kwargs):
     fig.savefig(path, dpi = dpi, **kwargs)
     if '.pdf' in path:
-        fig.savefig(path.replace('.pdf','.png'), dpi = dpi, **kwargs)
-        fig.savefig(path.replace('.pdf','_white.png'), dpi = dpi, facecolor='white', **kwargs)
+       fig.savefig(path.replace('.pdf','.png'), dpi = dpi, **kwargs)
+       fig.savefig(path.replace('.pdf','_white.png'), dpi = dpi, facecolor='white', **kwargs)
 
 
-def std_plot_limits(case, 
-        skip_ids=[], 
-        xrange=(5, 1e5), 
-        yrange=(1e-10,1e-1), 
+def std_plot_limits(case,
+        nature = '',
+        skip_ids=[],
+        xrange=(1e-3, 1e2),
+        yrange=(1e-10,1e-1),
         title=None, 
-        new_labelpos={}, 
+        new_labelpos={},
         new_color={},               # dic:  override defaul color for a set of limits 
         new_dash={},                # dic:  override defaul dash style for a set of limits (keys)
+        new_rotation={},            # dic:  override defaul rotation of the labels
         grid=False,                 # bool: plot a faint x and y grid 
-        color_fill = False,       # bool: filled regions have color
+        color_fill = False,         # bool: filled regions have color
         color_only = [],            # list: apply color only to these limits
         npoints_interp = int(1e5),  # int:  number of points to use when drawing the curves
-        suffix=''):                 # string: suffix of the plot name and file name 
+        suffix='',                  # string: suffix of the plot name and file name 
+        colormap='tab20'):          # what colormap to iterate over to color the limits
 
     fig, ax = std_fig(figsize=(8,4), ax_form=[0.1,0.125,0.88,0.81])
-    
-    x=np.geomspace(1,1e5, int(npoints_interp))
 
-    # sns.reset_orig()  # get default matplotlib styles back
+    x=np.geomspace(1e-3,1e2, int(npoints_interp))
+
     labelpos_dic={}
-    for i, limit in case.limits.iterrows():
-        ilabel, ival = np.nanargmin(limit.interp_func(x)), np.nanmin(limit.interp_func(x))
-        labelpos_dic[limit.id] = (x[ilabel]*0.9, ival/2.5)
-    color_dic = dict(zip(case.limits['id'],sns.color_palette('tab10', n_colors=len(list(case.limits.iterrows()))))) # a list of RGB tuples
-    dash_dic = dict(zip(case.limits['id'], (1+len(color_dic.keys()))*[(1,0)]))
-    
+    for id, limit in case.limits.iterrows():
+        if limit.interp_func is not None:
+            ilabel, ival = np.nanargmin(limit.interp_func(x)), np.nanmin(limit.interp_func(x))
+            labelpos_dic[id] = (x[ilabel]*0.9, ival/2.5)
+
+    n_colors = len(list(case.limits.iterrows()))
+    ##########
+    # find the average value of U, and determine the zorder based on that
+    df_order = 1/case.limits.ualpha4.apply(lambda x: np.mean(-np.log10(x), where=~np.isnan(x)) if x is not None else 1)
+    df_order_in_x = case.limits.m4.apply(lambda x: np.min(x[~np.isnan(x)]) if x is not None else 1)
+
+    sequence_of_colors = np.linspace(0, 1, n_colors)
+    sequence_of_colors = sequence_of_colors[np.argsort(df_order_in_x)]
+    color_dic = dict(zip(case.limits.index[np.argsort(df_order_in_x)], getattr(cm,colormap)(sequence_of_colors))) # a list of RGB tuples
+
+
+    dash_dic = dict(zip(case.limits.index, (1+len(color_dic.keys()))*[(1,0)]))
+    rot_dic = dict(zip(case.limits.index, (1+len(color_dic.keys()))*[0]))
+
     labelpos_dic.update(new_labelpos)
     color_dic.update(new_color)
     dash_dic.update(new_dash)
+    rot_dic.update(new_rotation)
 
-    for i, limit in case.limits.iterrows():
-        if limit.id not in skip_ids:
-            
+
+    for id, limit in case.limits.iterrows():
+        if (id not in skip_ids) & (limit.interp_func is not None):
+            if limit.ualpha4 is not None:
+                U_MEAN = df_order[f'{id}']/df_order.max()
+            else:
+                U_MEAN = 0
+
             if len(color_only)>0:
 
-                background_grey = lighten_color('lightgrey', 0.1)
-                if limit.id in color_only:
-                    c = color_dic[limit.id]
-                    LW = 1
-                    alpha=1
+                if id in color_only:
+                    contour_color= lighten_color(color_dic[id],1)
+                    fill_color = contour_color if color_fill else lighten_color('lightgrey', 0.3)
+                    label_color = color_dic[id]
+                    LW = 0.75
+                    ALPHA = 1
+                # else:
+                #     contour_color = 'black'
+                #     fill_color = lighten_color('lightgrey', 0.3)
+                #     label_color = 'black'
+                #     LW= 0.1#0.5
+                #     ALPHA = 1
                 else:
-                    c = 'black'
-                    LW=0.5
-                    alpha=1
+                    contour_color = 'grey'
+                    fill_color = lighten_color('lightgrey', 0)
+                    label_color = 'grey'
+                    LW= 0.5
+                    ALPHA = 1
             else:
-                background_grey = lighten_color('lightgrey', 0.3)
-                c = color_dic[limit.id]
-                alpha=1
-                LW = 1
+                contour_color= lighten_color(color_dic[id],1)
+                fill_color = contour_color if color_fill else lighten_color('lightgrey', 0.3)
+                label_color = color_dic[id]
+                ALPHA=1
+                LW = 0.75
+
+            # independently of what happened -- color the regions accordingly to color_fill or not at all.
 
             if (limit.year is None):
                 dash = (4,2)
+                contour_color = color_dic[id]
             else:
-                dash = dash_dic[limit.id]
-
+                dash = dash_dic[id]
+            
             label = fr'\noindent {limit.plot_label}'.replace("(",r" \noindent {\tiny \textsc{(").replace(")", r")} }").replace(r'\\', r"\vspace{-2ex} \\ ")
-            ax.plot(x, limit.interp_func(x), zorder=3, color=c, dashes=dash, lw=LW)
-            ax.plot(x, limit.interp_func_top(x), color=c, dashes=dash,zorder=2, lw=LW)
-            if ('bbn' not in limit.id) and (limit.year is not None):
-                ax.fill_between(x, limit.interp_func(x), x/x,  zorder=1, facecolor=background_grey, edgecolor='None', alpha=alpha)    
-                # ax.fill_between(x, limit.interp_func(x), limit.interp_func_top(x),  zorder=1, facecolor=background_grey, edgecolor='None', alpha=1)    
-            t = ax.annotate(label, xy=labelpos_dic[limit.id], xycoords='data', color=c, zorder=4, fontsize=7.5)
+            t = ax.annotate(label, xy=labelpos_dic[id], xycoords='data', color=label_color, zorder=100, fontsize=6.5, rotation=rot_dic[id])
             # t.set_bbox(dict(facecolor=background_grey, alpha=0.2, edgecolor='None'))
+            if limit.file_top == limit.file_bottom and limit.m4 is not None and limit.ualpha4 is not None:
+                ax.plot(limit.m4, limit.ualpha4, color=contour_color, dashes=dash, zorder=3, lw=LW)
+                
+                # Filling
+                if ('cosmo' in  id) or (limit.year is None):
+                    continue
+                else:
+                    ax.fill(limit.m4, limit.ualpha4, facecolor=fill_color, edgecolor='None', alpha=ALPHA, zorder=U_MEAN)
+
+
+            else:
+                if ('bebc_barouki'==id):
+                    ax.plot(x, limit.interp_func(x), color=contour_color, dashes=dash, zorder=3, lw=LW)
+                    ax.plot(x, limit.interp_func_top(x), color=contour_color, dashes=dash, zorder=3, lw=LW)
+                else:    
+                    ax.plot(x, limit.interp_func(x), color=contour_color, dashes=dash, zorder=3, lw=LW)
+                    ax.plot(x, limit.interp_func_top(x), color=contour_color, dashes=dash, zorder=3, lw=LW)
+                
+                # Filling
+                if ('cosmo' in  id) or (limit.year is None):
+                    #continue
+                    ax.fill_between(x, limit.interp_func(x), limit.interp_func_top(x), facecolor=fill_color, edgecolor='None', alpha=0.06, zorder=U_MEAN)
+
+                else:
+                    if limit.interp_func_top(x) is not None and limit.interp_func(x) is not None and U_MEAN is not None:
+                        ax.fill_between(x, limit.interp_func(x), limit.interp_func_top(x), facecolor=fill_color, edgecolor='None', alpha=ALPHA, zorder=U_MEAN)
+
 
     ax.set_yscale("log")
     ax.set_xscale("log")
 
+    ax.set_ylabel(fr"{case.latexflavor}", fontsize=18)
+    ax.set_xlabel(r"$M_N$ (GeV)", fontsize=18)
+    
+    ax.tick_params(which='both', bottom=True, top=True, left=True, right=True)
+    ax.tick_params(labelbottom=True, labeltop=False, labelleft=True, labelright=False)
 
-    ax.set_ylabel(fr"{case.latexflavor}")
-    ax.set_xlabel(fr"$m_N/$MeV")
-
-    major=np.array([1e-13,1e-12,1e-11, 1e-10,1e-9, 1e-8, 1e-7, 1e-6, 1e-5, 1e-4, 1e-3,1e-2,1e-1,1])
+    major=np.array([1e-13,1e-12,1e-11,1e-10,1e-9,1e-8,1e-7,1e-6,1e-5,1e-4,1e-3,1e-2,1e-1,1])
     minor=np.array([2,3,4,5,6,7,8,9])
     minor = np.array([m*minor for m in major]).flatten()[:-9]
     ax.set_yticks(major)
     ax.set_yticks(minor, minor=True)
+    
+    ax.tick_params(which='major', axis='x', length=6, direction='in')
+    ax.tick_params(which='minor', axis='x', length=3, direction='in')
+    ax.tick_params(which='major', axis='y', length=6, direction='in')
+    ax.tick_params(which='minor', axis='y', length=3, direction='in')
 
     if grid:
         ax.grid(axis='y', which='both', dashes=(6,1), alpha=0.25, c='black', lw=0.2)
@@ -144,7 +215,7 @@ def std_plot_limits(case,
     ax.set_xlim(*xrange)
     ax.set_title(title)
     
-    std_savefig(fig, path = f'plots/U{case.flavor}N{suffix}.pdf')
+    std_savefig(fig, path = f'plots/mixing/U{case.flavor}N{suffix}_{case.nature}.pdf')
     
     return fig, ax 
 
@@ -234,36 +305,50 @@ def plot_MB_vertical_region(ax, color='dodgerblue', label=r'MiniBooNE $1 \sigma$
     ax.vlines(xright,0,1e10, zorder=3, lw=0.5, color=color)
 
 
-def plot_closed_region(points, logx=False, logy=False):
+def get_ordered_closed_region(points, logx=False, logy=False):
     x,y = points
+
+    # check for nans
+    if np.isnan(points).sum()>0:
+        raise ValueError("NaN's were found in input data. Cannot order the contour.")
+
+    # check for repeated x-entries -- 
+    # this is an error because 
+    x, mask_diff = np.unique(x,return_index=True)
+    y = y[mask_diff]
+
     if logy:
         if (y==0).any():
             raise ValueError("y values cannot contain any zeros in log mode.")
         sy = np.sign(y)
         ssy = ((np.abs(y)<1)*(-1) + (np.abs(y)>1)*(1))
-        y  = ssy*np.log(y*sy)
+        y  = ssy*np.log10(y*sy)
     if logx:
         if (x==0).any():
             raise ValueError("x values cannot contain any zeros in log mode.")
         sx  = np.sign(x)
         ssx = ((x<1)*(-1) + (x>1)*(1))
-        x  = ssx*np.log(x*sx)
+        x  = ssx*np.log10(x*sx)
 
     points = np.array([x,y]).T
-
     points_s     = (points - points.mean(0))
     angles       = np.angle((points_s[:,0] + 1j*points_s[:,1]))
     points_sort  = points_s[angles.argsort()]
     points_sort += points.mean(0)
 
+    if np.isnan(points_sort).sum()>0:
+        raise ValueError("NaN's were found in sorted points. Cannot order the contour.")
+    # print(points.mean(0))
+    # return points_sort
     tck, u = splprep(points_sort.T, u=None, s=0.0, per=0, k=1)
-    u_new = np.linspace(u.min(), u.max(), len(points[:,0]))
-    x_new, y_new = splev(u_new, tck, der=0)
+    # u_new = np.linspace(u.min(), u.max(), len(points[:,0]))
+    x_new, y_new = splev(u, tck, der=0)
+    # x_new, y_new = splev(u_new, tck, der=0)
     
     if logx:
-        x_new = sx*np.exp(ssx*x_new) 
+        x_new = sx*10**(ssx*x_new) 
     if logy:
-        y_new = sy*np.exp(ssy*y_new) 
+        y_new = sy*10**(ssy*y_new) 
 
     return x_new, y_new
 
