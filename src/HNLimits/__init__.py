57cf62340924a5bcfe4a23dd8c4685ce914a0dc4
Matheus Hostert <mhostert@g.harvard.edu>
2024-06-10 14:08:46 -0400

    bump to v1.0.0

--- src/HNLimits/__init__.py
+++ src/HNLimits/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 # Default Google Sheet ID
-DEFAULT_SHEET = '1TIpmkgOa63-8Sy75qh0YutI5XdRtiClU3aquUdmjqpc'
+DEFAULT_SHEET = "1TIpmkgOa63-8Sy75qh0YutI5XdRtiClU3aquUdmjqpc"
